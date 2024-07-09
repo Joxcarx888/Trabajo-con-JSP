57217221110072a03c5fb68b8fbc7054a5ed26a0
Informatica <informatica@alumno.local>
2024-07-09 11:53:23 -0600

    Se agregaron dependencias, y se aplico las cosas necesarias para JPA

---
+++ src/main/java/org/josemorejon/webapp/util/JpaUtil.java
@@ -0,0 +1,26 @@
+package org.josemorejon.webapp.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class JpaUtil {
+    private static final EntityManagerFactory emf = buildEntityManagerFactory();
+    
+    public static EntityManagerFactory buildEntityManagerFactory(){
+        try{
+            return Persistence.createEntityManagerFactory("FormProductosWeb");
+        }catch(Exception e){
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+    
+    public static void close(){
+        emf.close();
+    }
+}
